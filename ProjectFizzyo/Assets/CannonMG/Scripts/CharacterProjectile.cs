18dfeb07821bfaa96abd63c6d3b2996f0583b78d
SamGainty <1600813@uad.ac.uk>
2018-11-13 15:51:17 +0000

    Set up folders for CoconutMG and created basic launch system for CircusMG

---
+++ ProjectFizzyo/Assets/CannonMG/Scripts/CharacterProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterProjectile : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, rb.velocity);
+	}
+}
---
+++ ProjectFizzyo/Assets/CannonMG/Scripts/VoidCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		if (col.name == "CharacterProjectile(Clone)") 
+		{
+			GameObject.Destroy (col.gameObject);
+		}
+	}
+}
