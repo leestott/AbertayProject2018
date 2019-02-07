13f72132363ce94f37933996120500d1090d0993
SamGainty <1600813@uad.ac.uk>
2019-02-07 15:46:07 +0000

    Iteration of Art and Animation | Redevelopment of controls and ReticleMovement Script

--- ProjectFizzyo/Assets/CoconutMG/Scripts/ReticleMovement.cs
+++ ProjectFizzyo/Assets/CoconutMG/Scripts/ReticleMovement.cs
@@ -15,18 +15,52 @@ public class ReticleMovement : MonoBehaviour {
     // Time the ball will take to reach coconuts.
     [Header("Travel time for the ball to reach the coconuts:")]
     public float travelTime;
+	public float maskTravelTime;
+	public float coconutTravelTime;
+	public float maxTravelTime;
+
+	public Transform groundPosition;
+
+	public float DEBUG = 0.0f;
+
+	AudioSource audioSource;
+	public AudioClip maskHitEffect;
+	public AudioClip groundHit;
+
+	public AudioClip[] coconutHitEffects;
+
+	public bool isMaskBlocking = false;
+	bool hasHitGround = false;
+	bool hasHitCoconut = false;
 
     [Header("The ball prefab:")]
     public GameObject ballPrefab;
+	GameObject ballProjectile;
 
 	public GameObject currentTarget;
+
+	public float ballUpwardVelocity;
+
+	public bool hasHit = false;
+	bool canPlay = false;
+
+	public float maxBallSize;
+	public float minBallSize;
+	public float minBallSizeFail;
+
+	public float currentTravelTime = 0.0f;
+	public bool isTravelling = false;
 
 	BreathRecogniser br = new BreathRecogniser();
 	private float breathPressure;
 
 	private float resetDelay = 3.0f;
 
-	public bool canPlay = false;
+	public bool hasStartedGame = false;
+
+	bool canReach = false;
+
+	bool hasLaunched = false;
 
 	BreathMetre breathMetre;
 
@@ -38,26 +72,143 @@ public class ReticleMovement : MonoBehaviour {
 	{
 		//Get reference to breath meter instance
 		breathMetre = FindObjectOfType<BreathMetre>();
+		audioSource = GameObject.FindObjectOfType<AudioSource> ();
 
 		br.BreathStarted += Br_BreathStarted;
 		br.BreathComplete += Br_BreathComplete;
 
 		reticle = GameObject.Find ("TargetReticle");
 	}
-		
+
 	void Update () 
 	{
-		AxisMovement ();
+		DEBUG = breathMetre.fillAmount;
 
-		if (Input.GetKeyDown (KeyCode.Space) || FizzyoFramework.Instance.Device.ButtonDown ())
+		if (canPlay)
 		{
-			if (currentTarget != null) 
+			if (!hasLaunched) 
 			{
-				Debug.Log ("Hit Coconut");
-				Rigidbody2D coconutRB = currentTarget.GetComponent<Rigidbody2D> ();
-				coconutRB.gravityScale = 1.0f;
-			}	
+				AxisMovement ();
+			}
+
+			if (Input.GetKeyDown (KeyCode.Space) || FizzyoFramework.Instance.Device.ButtonDown () && !hasLaunched)
+			{
+				if (breathMetre.fillAmount > 0.75f) 
+				{
+					canReach = true;
+					ballUpwardVelocity = 3.0f;
+				} 
+				else
+				{
+					canReach = false;
+					ballUpwardVelocity = breathMetre.fillAmount * 2.0f;
+				}
+				ballProjectile = Instantiate (ballPrefab, transform.position, Quaternion.identity) as GameObject;
+				ballProjectile.transform.localScale = new Vector3 (maxBallSize, maxBallSize, maxBallSize);
+				if (!canReach) 
+				{
+					SpriteRenderer ren = ballProjectile.GetComponent<SpriteRenderer> ();
+					ren.sortingLayerName = "ForeGround";
+					ren.sortingOrder = 2;
+				}
+				Rigidbody2D ballRB = ballProjectile.GetComponent<Rigidbody2D> ();
+				ballRB.velocity = new Vector2 (0, ballUpwardVelocity);
+				currentTravelTime = 0.0f;
+				isTravelling = true;
+				hasLaunched = true;	
+			}
+
+			if (hasLaunched) 
+			{
+				if (isTravelling && canReach) 
+				{
+					currentTravelTime += Time.deltaTime;
+					CalculateSize ();
+				}
+				if (isTravelling && !canReach)
+				{
+					currentTravelTime += Time.deltaTime;
+					CalculateSizeFail ();
+				}
+				if (currentTravelTime >= coconutTravelTime && !hasHit && canReach) 
+				{
+					hasHit = true;
+					if (isMaskBlocking) 
+					{
+						isTravelling = false;
+						currentTarget = null;
+						currentTravelTime = 0.0f;
+						Debug.Log ("HIT MASK");
+						audioSource.PlayOneShot (maskHitEffect);
+						StartCoroutine (ResetDelay ());
+					} 
+					else if (currentTarget != null && isTravelling) 
+					{
+						if (currentTarget.name == "Coconut")
+						{
+							isTravelling = false;
+							Debug.Log ("Hit Coconut");
+							if (!hasHitCoconut) 
+							{
+								hasHitCoconut = true;
+								int audioNumber = Random.Range (0, coconutHitEffects.Length);
+								Debug.Log ("Playing Coconut SFX: " + audioNumber);
+								audioSource.PlayOneShot (coconutHitEffects [audioNumber]);
+							}
+							Rigidbody2D coconutRB = currentTarget.GetComponent<Rigidbody2D> ();
+							coconutRB.gravityScale = 1.0f;
+							float randomTorque = Random.Range (-2.0f, 2.0f);
+							coconutRB.AddTorque (randomTorque);
+							Animator anim = currentTarget.GetComponent<Animator> ();
+							anim.SetBool ("isFalling", true);
+							StartCoroutine (ResetDelay ());
+						}
+					} 
+				}
+				if (currentTravelTime >= maxTravelTime) 
+				{
+					StartCoroutine (ResetDelay ());
+				}
+					//if (currentTravelTime >= travelTime && currentTarget != null) 
+					//{
+					//	isTravelling = false;
+					//Debug.Log ("Hit Coconut");
+					//Rigidbody2D coconutRB = currentTarget.GetComponent<Rigidbody2D> ();
+					//coconutRB.gravityScale = 1.0f;
+					//StartCoroutine (ResetDelay ());
+					//} 
+					//else
+					//{
+					//	CalculateSize ();
+					//}
+			}
+
+			if (ballProjectile != null && !hasHitGround) 
+			{
+				if (ballProjectile.transform.position.y <= groundPosition.position.y) 
+				{
+					hasHitGround = true;
+					AudioSource.PlayClipAtPoint (groundHit, audioSource.transform.position);
+				}
+			}
 		}
+
+		if (Input.GetKeyDown (KeyCode.Space) || FizzyoFramework.Instance.Device.ButtonDown () && !canPlay)
+		{
+			canPlay = true;
+		}
+	}
+
+	void Reset () 
+	{
+		isTravelling = false;
+		hasLaunched = false;
+		currentTravelTime = 0.0f;
+		hasHit = false;
+		hasHitGround = false;
+		hasHitCoconut = false;
+		GameObject.Destroy (ballProjectile);
+		breathMetre.fillAmount = 0.0f;
 	}
 
 	void AxisMovement ()
@@ -69,15 +220,46 @@ public class ReticleMovement : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D col) 
 	{
-		if (col.name == "Coconut") 
+		if (col.name == "TikiMask")
+		{
+			isMaskBlocking = true;
+			Debug.Log ("ENTERED MASK");
+		}
+		if (col.name == "Coconut")
 		{
 			currentTarget = col.gameObject;
 		}
+
+	}
+
+	void CalculateSize () 
+	{
+		float ballScale = maxBallSize + currentTravelTime * ((minBallSize - maxBallSize) / maxTravelTime);
+		ballProjectile.transform.localScale = new Vector3 (ballScale, ballScale, ballScale);
+	}
+
+	void CalculateSizeFail () 
+	{
+		float ballScale = maxBallSize + currentTravelTime * ((minBallSizeFail - maxBallSize) / maxTravelTime);
+		ballProjectile.transform.localScale = new Vector3 (ballScale, ballScale, ballScale);
 	}
 
 	void OnTriggerExit2D (Collider2D col)
 	{
-		currentTarget = null;
+		if (col.name == "Coconut") 
+		{
+			currentTarget = null;
+		}
+		if (col.name == "TikiMask")
+		{
+			isMaskBlocking = false;
+		}
+	}
+
+	IEnumerator ResetDelay () 
+	{
+		yield return new WaitForSeconds (2.0f);
+		Reset ();
 	}
 
 	private void Br_BreathStarted(object sender)
