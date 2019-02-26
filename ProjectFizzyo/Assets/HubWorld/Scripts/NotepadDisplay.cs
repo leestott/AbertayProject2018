b023322c34043da6a12f1a4457825d74d19df269
SamGainty <1600813@uad.ac.uk>
2019-02-26 16:35:07 +0000

    Fixed resolution issues for UWP| Screen resolution now set at (1920*1080)| Added additional control instructions for set selector and hubworld menu

---
+++ ProjectFizzyo/Assets/HubWorld/Scripts/NotepadDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotepadDisplay : MonoBehaviour {
+
+	public float displayTime;
+
+	Animator animator;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator> ();
+		StartCoroutine (displayDelay ());
+	}
+
+	IEnumerator displayDelay ()
+	{
+		yield return new WaitForSeconds (displayTime);
+		animator.SetBool ("shouldShow", true);
+	}
+}
---
+++ ProjectFizzyo/Assets/HubWorld/Scripts/PulseInstructions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseInstructions : MonoBehaviour {
+
+	// Starting value for the Lerp.
+	static float t = 0.0f;
+	// Animate the game object to scale from minimum to maximum and back.
+	private float minimum = 0.0F;
+	private float maximum = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+			// Increase the t interpolater for lerp using deltatime.
+			t += 1.0f * Time.deltaTime;
+
+			// When interpolater reaches one swap max and min to scale other way.
+			if (t > 1.0f)
+			{
+				float temp = maximum;
+				maximum = minimum;
+				minimum = temp;
+				t = 0.0f;
+			}
+
+			// Scale based on lerp.
+			transform.localScale = new Vector3(1 + Mathf.Lerp(minimum, maximum, t), 1 + Mathf.Lerp(minimum, maximum, t), 1 + Mathf.Lerp(minimum, maximum, t));
+		}
+	}
+
